123d1a6761cc13f0920dacaf2371bab81cb8798f
KaF <634121776@qq.com>
2023-04-02 18:17:39 +0800

    add operator of map for insert element

--- src/Test/treeTest.cpp
+++ src/Test/treeTest.cpp
@@ -2,26 +2,33 @@
 #include "iostream"
 
 int main(int argc, char** argv){
-    kTree::BST<int> tree;
-    tree.Insert(100);
-    tree.Insert(89);
-    tree.Insert(94);
-    tree.Insert(72);
-    tree.Insert(70);
-    tree.Insert(73);
-    tree.Insert(75);
-    tree.Insert(74);
+    //kTree::BST<int> tree;
+    //tree.Insert(100);
+    //tree.Insert(89);
+    //tree.Insert(94);
+    //tree.Insert(72);
+    //tree.Insert(70);
+    //tree.Insert(73);
+    //tree.Insert(75);
+    //tree.Insert(74);
 
-    tree.Remove(89);
-    kTree::AVL<int> avlTree;
-    avlTree.InsertAndBalance(10);
-    avlTree.InsertAndBalance(11);
-    avlTree.InsertAndBalance(4);
-    avlTree.InsertAndBalance(6);
-    avlTree.InsertAndBalance(2);
-    avlTree.InsertAndBalance(1);
-    avlTree.InsertAndBalance(3);
+    //tree.Remove(89);
+    //kTree::AVL<int> avlTree;
+    //avlTree.InsertAndBalance(10);
+    //avlTree.InsertAndBalance(11);
+    //avlTree.InsertAndBalance(4);
+    //avlTree.InsertAndBalance(6);
+    //avlTree.InsertAndBalance(2);
+    //avlTree.InsertAndBalance(1);
+    //avlTree.InsertAndBalance(3);
 
-    auto node = avlTree.Search(4);
-    avlTree.TestRotateRight(node);
+    ////avlTree.RefreshHeight(avlTree.GetRootNodePtr());
+    //auto node = avlTree.Search(4);
+
+    kTree::Map<int, int > map;
+    map.Insert(7,  1);
+    map.Insert(4,  1);
+    map.Insert(5,  1);
+
+    
 }
--- src/include/Tree.h
+++ src/include/Tree.h
@@ -104,6 +104,10 @@ namespace kTree
             return true;
         }
 
+        Node<T, NodeType>* GetRootNodePtr() {
+            return m_rootNode;
+        }
+
     protected:
         Node<T, NodeType>* m_rootNode{ nullptr };
         size_t m_size{0};
@@ -218,24 +222,39 @@ namespace kTree
         template<typename U, 
         std::enable_if_t<!std::is_pointer_v<U> && std::is_convertible_v<std::decay_t<U>, std::decay_t<T>>>* = nullptr>
         AVLNode<T>* InsertAndBalance(const U& value){
-            AVLNode<T>* returnPtr = nullptr;
-            BST<T, NodeType>::Insert(value);
+            AVLNode<T>* returnPtr = BST<T, NodeType>::Insert(value);
 
-            //update 
+            ComputeHeight(this->m_rootNode);
+            Balance(returnPtr);
             return returnPtr;
         }
 
         void TestRotateLeft(AVLNode<T>*& node) {
             RotateLeft(node);
         } 
+
         void TestRotateRight(AVLNode<T>*& node) {
             RotateRight(node);
         }
+
+        void RefreshHeight(AVLNode<T>* rootNode) {
+            //clearUp warning be used this pointer
+            ComputeHeight(rootNode);
+        }
     private:
 
-        void Refresh() {
-            //clearUp warning be used this pointer
-            this->m_rootNode->m_height++;
+        uint64_t ComputeHeight(AVLNode<T>* rootNode) {
+            if (rootNode == nullptr) {
+                return 0;
+            }
+            if (rootNode->leftNode == nullptr && rootNode->rightNode == nullptr) {
+                rootNode->m_height = 0;
+                return 0;
+            }
+            auto leftHeight = ComputeHeight(rootNode->leftNode);
+            auto rightHeight = ComputeHeight(rootNode->rightNode);
+            rootNode->m_height = leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+            return rootNode->m_height;
         }
 
         AVLNode<T>* RotateLeft(AVLNode<T>*& node) {
@@ -249,6 +268,9 @@ namespace kTree
             node->parent = rotateNode;
             rotateNode->leftNode = node;
 
+            if (fatherNode == nullptr) {
+                return rotateNode;
+            }
             if (*node->m_data < *fatherNode->m_data) {
                 fatherNode->leftNode = rotateNode;
             }
@@ -269,6 +291,9 @@ namespace kTree
             node->parent = rotateNode;
             rotateNode->rightNode = node;
 
+            if (fatherNode == nullptr) {
+                return rotateNode;
+            }
             if (*node->m_data < *fatherNode->m_data) {
                 fatherNode->leftNode = rotateNode;
             }
@@ -278,6 +303,236 @@ namespace kTree
             return rotateNode;
         }
 
+        void Balance(AVLNode<T>* node) {
+            while (node != nullptr) {
+                auto parent = node->parent;
+
+                auto difference = GetLeftRightDifference(node);
+                switch (difference) {
+                case 2:
+                    if (GetLeftRightDifference(node) >= 0) {
+                        RotateRight(node);
+                        FindRootNode(node);
+                        ComputeHeight(this->m_rootNode);
+                    }
+                    else {
+                        RotateLeft(node->leftNode);
+                        RotateRight(node);
+                        FindRootNode(node);
+                        ComputeHeight(this->m_rootNode);
+                    }
+                    break;
+                case -2:
+                    //���ڵ��� 
+                    if (GetLeftRightDifference(node) >= 0) {
+                        RotateLeft(node);
+                        FindRootNode(node);
+                        ComputeHeight(this->m_rootNode);
+                    }
+                    //���ڵ��Ҳ�����������
+                    else {
+                        RotateRight(node->rightNode);
+                        RotateLeft(node);
+                        FindRootNode(node);
+                        ComputeHeight(this->m_rootNode);
+                    }
+                    break;
+                default:
+                    break;
+                }
+                node = parent;
+            }
+        }
+
+        int64_t GetLeftRightDifference(const AVLNode<T>* node) const{
+            int64_t leftHeight = (node->leftNode == nullptr) ? -1 : node->leftNode->m_height;
+            int64_t rightHeight = (node->rightNode == nullptr) ? -1 : node->rightNode->m_height;
+
+            return leftHeight - rightHeight;
+        }
+
+        AVLNode<T>* FindRootNode(AVLNode<T>* node) {
+            if (node == nullptr) {
+                return nullptr;
+            }
+            while (node->parent != nullptr) {
+                node = node->parent;
+            }
+            this->m_rootNode = node;
+            return node;
+        }
     };
+
+    template<typename T, typename U>
+    struct  BRTree
+    {
+        BRTree(BRTree* _father, BRTree* _left, BRTree* _right, T _key, U _value, bool isleft, bool isred) : 
+            father(_father),  left(_left), right(_right), key(_key), value(_value), isLeft(isleft), isRed(isred){}
+        BRTree* father;
+        BRTree* left;
+        BRTree* right;
+
+        T key;
+        U value;
+
+        bool isLeft;
+        bool isRed;
+    };
+    
+    template<typename T, typename U>
+    class Map{
+    public:    
+        Map() = default;
+
+        using Tree = BRTree<T, U>;
+
+        void Insert(T key, U value) {
+            auto insertNode = Insert(key, value, root);
+            if (insertNode == nullptr) {
+                return;
+            }
+            BalanceTree(insertNode);
+            UpdateRoot(insertNode);
+        }
+
+        Tree* Insert(T key, U value, Tree* currentNode){
+            if(root == nullptr){
+                root = new Tree(nullptr, nullptr, nullptr, key, value, false, false);
+                return nullptr;
+            }
+            Tree* NeedInsert = currentNode;
+
+            if(key < NeedInsert->key){
+                if (NeedInsert->left == nullptr) {
+                    Tree* temp = new Tree(NeedInsert, nullptr, nullptr, key, value, true, true);
+                    NeedInsert->left = temp;
+                    return temp;
+                }
+                NeedInsert = NeedInsert->left;
+                return Insert(key, value, NeedInsert);
+            }
+            if (key > NeedInsert->key) {
+                if (NeedInsert->right == nullptr) {
+                    Tree* temp = new Tree(NeedInsert, nullptr, nullptr, key, value, false, true);
+                    NeedInsert->right = temp;
+                    return temp;
+                }
+                NeedInsert = NeedInsert->right;
+                return Insert(key, value, NeedInsert);
+            }
+            NeedInsert->value = value;
+            return nullptr;
+        }
+
+
+    private:
+        Tree* root{nullptr};
+
+        void LeftRotate(Tree* node) {
+            Tree* right = node->right;
+            Tree* rightLeft = right->left;
+            if (rightLeft != nullptr) {
+                rightLeft->isLeft = false;
+
+                rightLeft->father = node;
+            }
+            node->right = rightLeft;
+            right->father = node->father;
+            if (node->isLeft) {
+                if (node->father != nullptr) {
+                    node->father->left = right;
+                }
+                right->isLeft = true;
+            }
+            else {
+                if (node->father != nullptr) {
+                    node->father->right = right;
+                }
+                right->isLeft = false;
+            }
+            node->father = right;
+            right->left = node;
+            node->isLeft = true;
+        }
+
+        void RightRotate(Tree* node) {
+            Tree* left = node->left;
+            Tree* leftRight = left->right;
+            if (leftRight != nullptr) {
+                leftRight->isLeft = true;
+                leftRight->father = node;
+            }
+            node->left = leftRight;
+            left->father = node->father;
+            if (node->isLeft) {
+                if (node->father != nullptr) {
+                    node->father->left = left;
+                }
+                left->isLeft = true;
+            }
+            else {
+                if (node->father != nullptr) {
+                    node->father->right = left;
+                }
+                left->isLeft = false;
+            }
+            left->right = node;
+            node->father = left;
+            node->isLeft = false;
+            
+        }
+
+        void BalanceTree(Tree* node) {
+            if (node->father->isRed == false) {
+                return;
+            }
+            auto ppL = node->father->father->left;
+            auto ppR = node->father->father->right;
+            if (ppL != nullptr && ppR != nullptr &&
+                ppL->isRed && ppR->isRed) {
+                if (node->father->father != root) {
+                    node->father->father->isRed = true;
+                }
+                node->father->isRed = false;
+                return;
+            }
+
+            if (node->father->isLeft) {
+                if (!node->isLeft) {
+                    LeftRotate(node->father);
+                    node->isRed = false;
+                    node->father->isRed = true;
+                    RightRotate(node->father);
+                }
+                else {
+                    node->father->isRed = false;
+                    node->father->father->isRed = true;
+                    RightRotate(node->father->father);
+                }
+                return;
+            }
+
+            if (node->isLeft) {
+                RightRotate(node->father);
+                node->isRed = false;
+                node->father->isRed = true;
+                LeftRotate(node->father);
+            }
+            else {
+                node->father->isRed = false;
+                node->father->father->isRed = true;
+                LeftRotate(node->father->father);
+            }
+        }
+
+        void UpdateRoot(Tree* node) {
+            if (node->father == nullptr) {
+                root = node;
+                return;
+            }
+            UpdateRoot(node->father);
+        }
+    };
+
 } // namespace kTree
 #endif
